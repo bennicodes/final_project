df41455e141c73e16bcd572012aebfec1d46430e
Benedict Notoane <benni@MacBookPro.lan>
2024-11-22 21:19:22 +0100

    created and styled products page

---
+++ src/js/navbar.js
@@ -0,0 +1,15 @@
+// NAVBAR ---------------
+const toggleMenuButton = document.querySelector(".navbar__toggle-button");
+const navbarLinksContainer = document.querySelector(".navbar__links");
+const links = document.querySelectorAll(".navbar__link");
+
+toggleMenuButton.addEventListener("click", () => {
+  navbarLinksContainer.classList.toggle("navbar__links--active");
+});
+
+links.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    links.forEach((link) => link.classList.remove("navbar__link--active"));
+    e.currentTarget.classList.add("navbar__link--active");
+  });
+});
