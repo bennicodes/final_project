7c19e6584debee2864e1c115b46af9ea0379a6c9
Benedict Notoane <benni@MacBookPro.lan>
2024-11-26 22:56:06 +0100

    created js file for products

---
+++ src/js/product-page.js
@@ -0,0 +1,152 @@
+// Import Products from products.js -------
+import {
+  airpodsProducts,
+  appleWatchProducts,
+  ipadProducts,
+  iphoneProducts,
+  macProducts,
+} from "./products.js";
+// Product categories
+const productCategories = ["iphone", "mac", "ipad", "airpods", "watch"];
+const productData = {
+  iphone: iphoneProducts,
+  mac: macProducts,
+  ipad: ipadProducts,
+  airpods: airpodsProducts,
+  watch: appleWatchProducts,
+};
+
+const main = document.querySelector("main");
+
+const popupContainer = document.querySelector(".popup__container");
+const popupHeading = document.querySelector(".popup__heading");
+const popupImage = document.querySelector(".popup__product-image");
+const popupSpecs = document.querySelector(".popup__product-list");
+const popupBuyButton = document.querySelector("#popup__buy-button");
+const closePopupButton = document.querySelector(".popup__close-button");
+
+// Function to open the popup
+function openPopup(product) {
+  if (!popupContainer || !popupSpecs) {
+    console.error("Popup container or specs list not found.");
+    return;
+  }
+
+  //   Popup buy button
+  popupBuyButton.textContent = "Buy";
+
+  // Update product details in the popup
+  popupHeading.textContent = product.name;
+  popupImage.src = product.image;
+  popupImage.alt = product.alt;
+
+  // Clear existing specs and add new ones
+  popupSpecs.innerHTML = "";
+
+  product.specs.forEach((spec) => {
+    const specItem = document.createElement("li");
+    specItem.textContent = spec;
+    specItem.classList.add("popup__product-list-specs");
+
+    // Append each spec as a list item
+    popupSpecs.appendChild(specItem);
+  });
+
+  // Show the popup
+  popupContainer.classList.add(
+    "popup__container--active",
+    "popup__container-overlay--active"
+  );
+}
+
+// TODO: Add learn more button when hovering
+
+// Close popup
+function closePopupAction() {
+  popupContainer.classList.remove(
+    "popup__container--active",
+    "popup__container-overlay--active"
+  );
+}
+
+// Function to close the popup
+function closePopup() {
+  popupContainer.classList.remove(
+    "popup__container--active",
+    "popup__container-overlay--active"
+  );
+}
+
+// Add event listener to the close button
+if (closePopupButton) {
+  closePopupButton.addEventListener("click", closePopup);
+}
+
+// Close the popup wehn user clicks outside of popup
+popupContainer.addEventListener("click", (e) => {
+  if (e.target === popupContainer) {
+    closePopup();
+  }
+});
+
+// Sort products to the right category -------
+productCategories.forEach((category) => {
+  const section = document.querySelector(`.${category}__product-section`);
+  //   Clear existing content
+  section.innerHTML = "";
+
+  // Get the product list for the current category
+  const products = productData[category];
+
+  // Loop through the products and create cards
+  products.forEach((product) => {
+    // Product container
+    const productContainer = document.createElement("div");
+    productContainer.classList.add("product__section-container");
+    section.appendChild(productContainer);
+
+    // Product card
+    const productCard = document.createElement("button");
+    productCard.classList.add("product__card");
+    productContainer.appendChild(productCard);
+
+    // Product name
+    const productName = document.createElement("h3");
+    productName.textContent = product.name;
+    productName.classList.add("product__card-heading");
+    productCard.appendChild(productName);
+
+    // Product image
+    const productImageContainer = document.createElement("div");
+    productImageContainer.classList.add("product__card-image-container");
+    productCard.appendChild(productImageContainer);
+
+    const productImage = document.createElement("img");
+    productImage.src = product.image;
+    productImage.alt = product.alt;
+    productImage.classList.add("product__card-image");
+    productImageContainer.appendChild(productImage);
+
+    // Bottom card container -------------
+    const bottomCardContainer = document.createElement("div");
+    bottomCardContainer.classList.add("bottom__card-container");
+    productCard.appendChild(bottomCardContainer);
+
+    // Product price
+    const productPrice = document.createElement("p");
+    productPrice.textContent = `$${product.price}`;
+    bottomCardContainer.appendChild(productPrice);
+
+    // Buy button
+    const buyButton = document.createElement("button");
+    buyButton.href = product.buyButton;
+    buyButton.textContent = "Buy";
+    buyButton.classList.add("product__buy-button");
+    bottomCardContainer.appendChild(buyButton);
+
+    // Event listener to open the popup
+    productCard.addEventListener("click", () => {
+      openPopup(product);
+    });
+  });
+});
--- src/js/products.js
+++ src/js/products.js
@@ -1,5 +1,5 @@
 // Products
-const iphoneProducts = [
+export const iphoneProducts = [
   {
     name: "iPhone 16 Pro & Pro Max",
     price: 999.0,
@@ -32,7 +32,7 @@ const iphoneProducts = [
     image: "../assets/img/iphone-SE-removebg-preview.png",
     alt: "iPhone SE",
     specs: [
-      "4.7-inch Retina HD display footnote ¹ that’s bright, colorful, and sharp.",
+      "4.7-inch Retina HD display that’s bright, colorful, and sharp.",
       "A15 Bionic, the same superpowerful chip that’s in iPhone 13.",
       "5G for fast downloads and high-quality streaming.",
       "Smarter camera makes automatic adjustments so faces, places, everything looks fabulous",
@@ -40,7 +40,7 @@ const iphoneProducts = [
     buyButton: "Buy",
   },
 ];
-const macProducts = [
+export const macProducts = [
   {
     name: "MacBook Pro ",
     price: 1599.0,
@@ -82,7 +82,7 @@ const macProducts = [
     buyButton: "Buy",
   },
 ];
-const ipadProducts = [
+export const ipadProducts = [
   {
     name: "iPad Pro ",
     price: 1099.0,
@@ -123,7 +123,7 @@ const ipadProducts = [
     buyButton: "Buy",
   },
 ];
-const airpodsProducts = [
+export const airpodsProducts = [
   {
     name: "AirPods 4 ",
     price: 129.0,
@@ -166,7 +166,7 @@ const airpodsProducts = [
     buyButton: "Buy",
   },
 ];
-const appleWatchProducts = [
+export const appleWatchProducts = [
   {
     name: "Apple Watch Series 10",
     price: 399.0,
@@ -209,148 +209,3 @@ const appleWatchProducts = [
     buyButton: "Buy",
   },
 ];
-
-// Product categories
-const productCategories = ["iphone", "mac", "ipad", "airpods", "watch"];
-const productData = {
-  iphone: iphoneProducts,
-  mac: macProducts,
-  ipad: ipadProducts,
-  airpods: airpodsProducts,
-  watch: appleWatchProducts,
-};
-
-const main = document.querySelector("main");
-
-const popupContainer = document.querySelector(".popup__container");
-const popupHeading = document.querySelector(".popup__heading");
-const popupImage = document.querySelector(".popup__product-image");
-const popupSpecs = document.querySelector(".popup__product-list");
-const popupBuyButton = document.querySelector("#popup__buy-button");
-const closePopupButton = document.querySelector(".popup__close-button");
-
-// Function to open the popup
-function openPopup(product) {
-  if (!popupContainer || !popupSpecs) {
-    console.error("Popup container or specs list not found.");
-    return;
-  }
-
-  //   Popup buy button
-  popupBuyButton.textContent = "Buy";
-
-  // Update product details in the popup
-  popupHeading.textContent = product.name;
-  popupImage.src = product.image;
-  popupImage.alt = product.alt;
-
-  // Clear existing specs and add new ones
-  popupSpecs.innerHTML = "";
-
-  product.specs.forEach((spec) => {
-    const specItem = document.createElement("li");
-    specItem.textContent = spec;
-    specItem.classList.add("popup__product-list-specs");
-
-    // Append each spec as a list item
-    popupSpecs.appendChild(specItem);
-  });
-
-  // Show the popup
-  popupContainer.classList.add(
-    "popup__container--active",
-    "popup__container-overlay--active"
-  );
-}
-
-// TODO: Add learn more button when hovering
-
-// Close popup
-function closePopupAction() {
-  popupContainer.classList.remove(
-    "popup__container--active",
-    "popup__container-overlay--active"
-  );
-}
-
-// Function to close the popup
-function closePopup() {
-  popupContainer.classList.remove(
-    "popup__container--active",
-    "popup__container-overlay--active"
-  );
-}
-
-// Add event listener to the close button
-if (closePopupButton) {
-  closePopupButton.addEventListener("click", closePopup);
-}
-
-// Close the popup wehn user clicks outside of popup
-popupContainer.addEventListener("click", (e) => {
-  if (e.target === popupContainer) {
-    closePopup();
-  }
-});
-
-// Sort products to the right category -------
-productCategories.forEach((category) => {
-  const section = document.querySelector(`.${category}__product-section`);
-  //   Clear existing content
-  section.innerHTML = "";
-
-  // Get the product list for the current category
-  const products = productData[category];
-
-  // Loop through the products and create cards
-  products.forEach((product) => {
-    // Product container
-    const productContainer = document.createElement("div");
-    productContainer.classList.add("product__section-container");
-    section.appendChild(productContainer);
-
-    // Product card
-    const productCard = document.createElement("button");
-    productCard.classList.add("product__card");
-    productContainer.appendChild(productCard);
-
-    // Product name
-    const productName = document.createElement("h3");
-    productName.textContent = product.name;
-    productName.classList.add("product__card-heading");
-    productCard.appendChild(productName);
-
-    // Product image
-    const productImageContainer = document.createElement("div");
-    productImageContainer.classList.add("product__card-image-container");
-    productCard.appendChild(productImageContainer);
-
-    const productImage = document.createElement("img");
-    productImage.src = product.image;
-    productImage.alt = product.alt;
-    productImage.classList.add("product__card-image");
-    productImageContainer.appendChild(productImage);
-
-    // Bottom card container -------------
-    const bottomCardContainer = document.createElement("div");
-    bottomCardContainer.classList.add("bottom__card-container");
-    productCard.appendChild(bottomCardContainer);
-
-    // Product price
-    const productPrice = document.createElement("p");
-    productPrice.textContent = `$${product.price}`;
-    bottomCardContainer.appendChild(productPrice);
-
-    // Buy button
-    const buyButton = document.createElement("button");
-    buyButton.href = product.buyButton;
-    buyButton.textContent = "Buy";
-    buyButton.classList.add("product__buy-button");
-    bottomCardContainer.appendChild(buyButton);
-
-    // Event listener to open the popup
-    productCard.addEventListener("click", () => {
-      openPopup(product);
-    });
-  });
-});
